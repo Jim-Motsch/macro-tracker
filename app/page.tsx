1e1a4c063a9a0227368ad103d8b7b9377a1e771a
Jim_Motsch <jimmymotsch@gmail.com>
2026-06-06 10:51:23 -0500

    add Clerk auth with protected routes

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,10 @@
-export default function Home(){
-  return <h1>Macro Tracker</h1>;
+import { UserButton } from '@clerk/nextjs'
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Macro Tracker</h1>
+      <UserButton />
+    </div>
+  )
 }
---
+++ middleware.ts
@@ -0,0 +1,20 @@
+
+
+import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
+//Defines what routes are public
+const isPublicRoute = createRouteMatcher(['/sign-in(.*)', '/sign-up(.*)'])
+
+export default clerkMiddleware(async (auth, request) => {
+//This says - sign in and sign up pages are public, everyone can see them
+//protect is from clerkMiddleWare library
+  if (!isPublicRoute(request)) {
+    await auth.protect()
+  }
+})
+
+export const config = {
+  matcher: [
+    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+    '/(api|trpc)(.*)',
+  ],
+}
