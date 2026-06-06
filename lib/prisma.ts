e6c2e05db596fbfc44cd24b6a9e61d97f7a10072
Jim_Motsch <jimmymotsch@gmail.com>
2026-06-06 12:06:15 -0500

    add prisma

---
+++ lib/prisma.ts
@@ -0,0 +1,7 @@
+import { PrismaClient } from '@/app/generated/prisma'
+import { PrismaNeon } from '@prisma/adapter-neon'
+
+const adapter = new PrismaNeon({ connectionString: process.env.DATABASE_URL! })
+const prisma = new PrismaClient({ adapter })
+
+export default prisma
